5215b840ed936b714624ee5656b870cea065a958
wfaler <wille.faler@gmail.com>
2013-04-12 23:49:16 +0100

    some more tests

--- test/defdata/core_test.clj
+++ test/defdata/core_test.clj
@@ -1,9 +1,15 @@
 (ns defdata.core-test
   (:use midje.sweet)
-  (:require defdata.core-defs)
+  (:require defdata.core-defs
+            defdata.core)
   (:import java.net.FileNameMap))
 
 ;; FileThing is defined in defdata/core_defs.clj to ensure things work across namespaces
 (fact (instance? FileNameMap (defdata.core-defs/new-FileThing 5))=> true)
 (fact (instance? defdata.core_defs.FileThing (defdata.core-defs/new-FileThing 5)) => true)
 (fact (defdata.core-defs/new-FileThing "foo") => (throws AssertionError))
+
+(fact (defdata.core/non-empty-string? nil) => false)
+(fact (defdata.core/non-empty-string? "") => false)
+(fact (defdata.core/non-empty-string? "  ") => false)
+(fact (defdata.core/non-empty-string? "i") => true)
