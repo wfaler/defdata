06cc777378bc064b886aab302f891956af60a4d1
wfaler <wille.faler@gmail.com>
2013-04-12 23:19:19 +0100

    added comments

--- test/defdata/core_defs.clj
+++ test/defdata/core_defs.clj
@@ -3,9 +3,9 @@
   (:import java.net.FileNameMap))
 
 ;; different namespace to ensure the code works across namespaces
-
+;; will generate a defrecord FileThing and a creation defn new-FileThing that asserts all inputs are valid
 (defdata FileThing [a]
-  [(number? a)]
-  FileNameMap
-  (getContentTypeFor [this fileName] (str a "-" fileName)))
+  [(number? a)] ;; vector with validation assertions against creation arguments
+  FileNameMap ;; protocols/interfaces implemented
+  (getContentTypeFor [this fileName] (str a "-" fileName))) ;; implemented protocol/interface methods
 
--- test/defdata/core_test.clj
+++ test/defdata/core_test.clj
@@ -3,6 +3,7 @@
   (:require defdata.core-defs)
   (:import java.net.FileNameMap))
 
+;; FileThing is defined in defdata/core_defs.clj to ensure things work across namespaces
 (fact (instance? FileNameMap (defdata.core-defs/new-FileThing 5))=> true)
 (fact (instance? defdata.core_defs.FileThing (defdata.core-defs/new-FileThing 5)) => true)
 (fact (defdata.core-defs/new-FileThing "foo") => (throws AssertionError))
