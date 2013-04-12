c0c7dd302728bca60c27f2c6ecf5829b066972db
wfaler <wille.faler@gmail.com>
2013-04-12 22:07:13 +0100

    initial stab at macro

--- src/defdata/core.clj
+++ src/defdata/core.clj
@@ -1,6 +1,19 @@
 (ns defdata.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defmacro defdata
+  ([type-name field-list constraints & etc]
+;;`(defrecord2 ~type-name ~field-list
+;; invoke defrecord2 with default constructor function name
+;;~(symbol (str "new-" (.toLowerCase (str type-name))))))
+     `(do
+        (defrecord ~type-name ~field-list ~@etc)
+        (defn ~(symbol (str "new-" type-name)) ~field-list
+          (println "hello world"))
+;; define the constructor functions
+;; setup pprinting
+        )))
+
+
+(defn non-empty-string? [s]
+  (and (string? s) (< 0 (.length (.trim s)))))
